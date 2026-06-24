4fa64236877b1f9b2c5af5c7acdb598ebea57583
Suresh Nunna <suresh.nunna@hcltech.com>
2026-06-24 16:12:11 +0530

    removed provider

--- provider.tf
+++ provider.tf
@@ -7,10 +7,6 @@ terraform{
   }
 }
 provider "azurerm" {
-  subscription_id = "73de6881-2d08-471d-9c59-6f32608206d3"
-  tenant_id       = "4f31d5a0-4f8c-4fe4-a9a3-7398b5099a99"
-  client_id       = "e5e697ac-4a01-4135-8d9a-62af95afb99a"
-#  client_secret   = var.CLIENT_SECRET
   features {
     resource_group {
       prevent_deletion_if_contains_resources = false
