ee00d60b91f38ca751018c9ea22396b00566d14d
Suresh Nunna <suresh.nunna@hcltech.com>
2026-06-24 16:02:27 +0530

    testing OIDC

--- variables.tf
+++ variables.tf
@@ -2,8 +2,10 @@ variable "rgname" {
   type = string
 
 }
+/*
 variable "CLIENT_SECRET" {
   description = "Azure Service Principal client secret"
   type        = string
   sensitive   = true
 }
+*/
